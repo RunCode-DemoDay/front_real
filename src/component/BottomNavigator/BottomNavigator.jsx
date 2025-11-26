db200e87641968066fc7fea2c52989424c1c656f
ryankim00 <ryankim00@naver.com>
2025-11-26 23:10:57 +0900

    search

--- src/component/BottomNavigator/BottomNavigator.jsx
+++ src/component/BottomNavigator/BottomNavigator.jsx
@@ -27,11 +27,12 @@ const BottomNavigator = () => {
     const location = useLocation(); 
 
     const navItems = [
-        { name: '저장', key: 'save', path: '/saved' },
-        { name: '홈', key: 'home', path: '/home' },
-        { name: '런코드', key: 'record', path: '/archiving' }, 
-        { name: '마이', key: 'my', path: '/mypage' },      
-    ];
+      { name: "저장", key: "save", path: "/saved" },
+      { name: "홈", key: "home", path: "/home" },
+      { name: "런코드", key: "record", path: "/archiving" },
+      { name: "마이", key: "my", path: "/mypage" },
+      { name: "홈", key: "home", path: "/search" },
+    ];
     
     const handleTabClick = (path) => {
         navigate(path);
